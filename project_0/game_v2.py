115014ee91dcda7f925ec5cb13ae7dddbaaeaed0
Tatpraded <monrepo@gmail.com>
2022-11-06 15:45:13 +0300

    edit.md

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,6 +1,3 @@
-"""Игра угадай число. 
-Компьютер сам загадывает и угадывает число
-"""
 import numpy as np
 def random_predict(number:int=1) -> int:
     """Компьютер угадывает рандомное число
@@ -16,11 +13,12 @@ def random_predict(number:int=1) -> int:
     while True:
         count += 1
         predict_number = np.random.randint(1, 101) # предполагаемое число
-        predict_number = (max_number + min_number) // 2 # расчитываем среднее значение между min_number и max_number 
-        if predict_number > number: # сравниваем загаданное значение с предполагаемым predict_number... 
+        predict_number = (max_number + min_number) // 2 # расчитываем среднее значение между min_number и max_number
+        if predict_number > number: # сравниваем загаданное значение с предполагаемым predict_number..
             max_number = predict_number - 1 # и если оно больше уменьшаем его
-        elif predict_number < number: # сравниваем загаданное значение с предполагаемым predict_number...
+        elif predict_number < number: # сравниваем загаданное значение с предполагаемым predict_number..
             min_number = predict_number + 1 # и если оно меньше увеличиваем его
+        else:
             break # конец игры и выход из цикла
     return(count)
 
@@ -31,7 +29,6 @@ def score_game(random_predict) -> int:
     Returns:
         int: среднее количество попыток
     """
-
     count_ls = []
     np.random.seed(1)  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
@@ -39,9 +36,6 @@ def score_game(random_predict) -> int:
     for number in random_array:
         count_ls.append(random_predict(number))
     score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток")
-    
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+
 score_game(random_predict)
-
-
-   
